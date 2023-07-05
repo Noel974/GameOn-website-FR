3239724cad63812b12ad2c4953cbe7332e2241ac
noel <noelantoine974@outlook.fr>
2023-07-05 15:30:24 +0400

    modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,6 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
-
+import { oufermModal } from './modal/ou-ferm.js';
+// Utilisation de la fonction ouvrirModal dans le fichier modal.js
+oufermModal();
+import { confirmation } from './modal/confirmation.js';
+// Utilisation de la fonction confirmation dans le fichier modal.js pour le message de confirmation
+confirmation();
---
+++ starterOnly/modal/ou-ferm.js
@@ -0,0 +1,24 @@
+export function oufermModal() {
+    const modalBtn = document.querySelectorAll(".modal-btn");
+    const closeBtn = document.querySelector(".close");
+    const modalbg = document.querySelector(".bground");
+
+    /**
+     * Fonction pour ouvrir la modale
+     */
+    function launchModal() {
+        modalbg.style.display = "block";
+    }
+
+    modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+
+    /**
+     * Fonction pour fermer la modale
+     */
+    function closeModal() {
+        modalbg.style.display = "none";
+    }
+
+    closeBtn.addEventListener("click", closeModal);
+}
